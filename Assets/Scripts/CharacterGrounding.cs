877a53535fa808e2c455018f456dba4e8bde735b
Bskasan <b.kasan@yandex.com>
2023-10-16 13:23:47 +0300

    up

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -4,14 +4,26 @@ public class CharacterGrounding : MonoBehaviour
 {
     [SerializeField] private Transform _leftFoot;
     [SerializeField] private Transform _rightFoot;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private int layerMask;
 
-    void Start()
-    {
-        
-    }
+    private bool isGrounded;
 
-    void Update()
+    private void Update()
     {
-                                
+        var raycastHit = Physics2D.Raycast(
+            _leftFoot.position,
+            Vector2.down,
+            maxDistance,
+            layerMask);
+
+        if (raycastHit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false; 
+        }
     }
 }
