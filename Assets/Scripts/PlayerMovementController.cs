37d0596178b2f9c42a4b84b16e7cdfdfdfd3aae1
Bskasan <b.kasan@yandex.com>
2023-10-16 12:59:37 +0300

    up
    jump method

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,17 +1,29 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
     [Header("Player Movement")]
     [SerializeField] private float _moveSpeed = 2f;
+    [SerializeField] private float _jumpForce = 400f;
 
-    private void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3 (horizontal, vertical);
+        
+        Vector3 movement = new Vector3 (horizontal, 0);
 
         transform.position += movement * Time.deltaTime * _moveSpeed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(Vector2.up * _jumpForce);
     }
 }
