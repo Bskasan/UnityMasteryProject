3a0569bf35a35498eb3c7f533b4a975101d05cd6
Bskasan <b.kasan@yandex.com>
2023-10-17 21:34:31 +0300

    kill on touch script done

---
+++ Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var playerMovementController = collision.collider.GetComponent<PlayerMovementController>();
+        if (playerMovementController != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        return;
+    }
+}
