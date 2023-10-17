be7b78187d5f5462cf1b45831ec0481034aeb001
Bskasan <b.kasan@yandex.com>
2023-10-18 00:08:09 +0300

    added cinemachine shake method

---
+++ Assets/Scripts/CinemachineShake.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera _virtualCamera;
+    private float _shakeTimer;
+
+    private void Awake()
+    {
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensity, float time) 
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        _shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if (_shakeTimer > 0)
+        {
+            _shakeTimer -= Time.deltaTime;
+            if( _shakeTimer <= 0 ) 
+            {
+                // Timer over!
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }           
+    }
+}
