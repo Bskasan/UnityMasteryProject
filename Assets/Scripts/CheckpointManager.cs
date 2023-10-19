f12950cc27d2d5d7dc9009a6225daa6946b26ee1
Bskasan <b.kasan@yandex.com>
2023-10-19 22:30:56 +0300

    last or default method

--- Assets/Scripts/CheckpointManager.cs
+++ Assets/Scripts/CheckpointManager.cs
@@ -1,8 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CheckpointManager : MonoBehaviour
 {
-   
+    private Checkpoint[] checkpoints;
+
+    private void Start()
+    {
+        checkpoints = GetComponentsInChildren<Checkpoint>();
+    }
+
+    public Checkpoint GetLastCheckpointThatWasPassed()
+    {
+        // Don't use too much, it's going to cause garbage!
+        return checkpoints.LastOrDefault();
+    }
 }
