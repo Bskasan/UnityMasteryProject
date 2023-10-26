86c4257fb72a3b6d8eadaa2e3ddc6f37ea70aa75
Bskasan <b.kasan@yandex.com>
2023-10-26 22:56:31 +0300

    up

---
+++ Assets/Scripts/UICoinImage.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICoinImage : MonoBehaviour
+{
+    private Animator _animator;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        GameManager.Instance.OnCoinsChanged += Pulse;
+    }
+
+    private void Pulse(int coins)
+    {
+        _animator.SetTrigger("Pulse");
+    }
+
+}
