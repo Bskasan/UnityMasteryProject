ff556590ede2e91945001cf189706442105d29d7
Bskasan <b.kasan@yandex.com>
2023-10-30 20:59:31 +0200

    up

---
+++ Assets/Scripts/SawBlade.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : MonoBehaviour
+{
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private Transform sawBladeSprite;
+
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+}
