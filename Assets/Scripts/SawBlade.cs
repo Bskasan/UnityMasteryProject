d7aa4ed946207e22cf4144b93c0cb7874c6a3115
Bskasan <b.kasan@yandex.com>
2023-10-30 21:55:39 +0200

    Added speed to sawblade

--- Assets/Scripts/SawBlade.cs
+++ Assets/Scripts/SawBlade.cs
@@ -7,13 +7,17 @@ public class SawBlade : MonoBehaviour
     [SerializeField] private Transform _start;
     [SerializeField] private Transform _end;
     [SerializeField] private Transform _sawBladeSprite;
+    [SerializeField] private float _speed;
 
     private float _positionPercent;
     private int _direction = 1;
 
     private void Update()
     {
-        _positionPercent += Time.deltaTime * _direction;
+        float distance = Vector3.Distance(_start.position, _end.position);
+        float speedForDistance = _speed / distance;
+
+        _positionPercent += Time.deltaTime * _direction * speedForDistance;
 
         _sawBladeSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent);
 
