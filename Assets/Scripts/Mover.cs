bfa1afb335c161825320315fa351e061021c24c4
Bskasan <b.kasan@yandex.com>
2023-10-31 18:44:36 +0200

    up

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawBlade : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform _start;
     [SerializeField] private Transform _end;
-    [SerializeField] private Transform _sawBladeSprite;
+    [SerializeField] private Transform _sprite;
     [SerializeField] private float _speed;
 
     private float _positionPercent;
@@ -19,7 +19,7 @@ public class SawBlade : MonoBehaviour
 
         _positionPercent += Time.deltaTime * _direction * speedForDistance;
 
-        _sawBladeSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent);
+        _sprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent);
 
         if (_positionPercent >= 1 && _direction == 1)
         {
