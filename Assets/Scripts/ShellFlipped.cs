35dad1b458b7aa84abc0070f8fa4c32434c43ee4
Bskasan <b.kasan@yandex.com>
2023-11-07 12:01:19 +0200

    up

--- Assets/Scripts/ShellFlipped.cs
+++ Assets/Scripts/ShellFlipped.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class ShellFlipped : MonoBehaviour
 {
+
+    [SerializeField] private float _shellSpeed = 5f;
+
     private Collider2D _collider;
     private Rigidbody2D _rigidbody2D;
 
+    private Vector2 _direction;
+    
     private void Awake()
     {
         _collider = GetComponent<Collider2D>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.WasHitByPlayer())
+        {
+            if (_direction.magnitude == 0) // if it's not moving
+            {
+                LaunchShell(collision);
+            }
+        }
     }
 
 }
