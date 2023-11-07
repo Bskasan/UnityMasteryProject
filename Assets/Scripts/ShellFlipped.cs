691f7a5ee515c0c9e7ccbb7d5a3d44bf77f1461d
Bskasan <b.kasan@yandex.com>
2023-11-07 12:11:20 +0200

    flipping snail and moving it when player hit from top done

--- Assets/Scripts/ShellFlipped.cs
+++ Assets/Scripts/ShellFlipped.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShellFlipped : MonoBehaviour
@@ -17,6 +14,11 @@ public class ShellFlipped : MonoBehaviour
     {
         _collider = GetComponent<Collider2D>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_direction.x * _shellSpeed, _rigidbody2D.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
